7dc807c05393246ec9c290ed1ced3322acd81c5a
masterpranay1 <rajpranaychottu@gmail.com>
2022-02-20 14:44:07 +0530

    Added rounting in Header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,15 +3,17 @@ import {ReactComponent as Moon} from './moon.svg';
 
 import { toggleTheme } from "../../redux/theme/theme.reducer";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import "./header.styles.scss";
 
 const Header = () => {
   const dispatch = useDispatch();
   const hide = useSelector((state) => state.theme.hidden);
+  const navigate = useNavigate();
   return (
     <div className="header">
-      <h1 className="h1">Where is the world?</h1>
+      <h1 className="h1" onClick={() => navigate('/')}>Where is the world?</h1>
       <div className="theme-switcher" onClick={() => dispatch(toggleTheme())}>
         {!hide ? (
           <img className="icon" src="https://img.icons8.com/external-bearicons-glyph-bearicons/64/000000/external-moon-halloween-bearicons-glyph-bearicons.png" />
